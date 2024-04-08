e642ade16661d09593071a7b5af0ac70df533805
fabian.wilms <fabian.wilms@muenchen.de>
2024-04-08 10:32:52 +0200

    fix(vite): fix entrypoint

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/components/index.ts'),
       name: 'muc-patternlab-vue',
       // the proper extensions will be added
       fileName: 'muc-patternlab-vue',
