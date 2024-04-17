aaf74af34ace8c18052ebe72745dd89ffcdad95d
jannik.lange <jannik.lange@muenchen.de>
2024-04-17 22:26:48 +0200

    :bento: Added icons for callout - replace with icon component in the future #26

---
+++ src/components/Callout/icon-warning.vue
@@ -0,0 +1,22 @@
+<!-- This icon is from <https://github.com/Templarian/MaterialDesign>, distributed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0) license-->
+<template>
+  <svg
+    aria-hidden="true"
+    role="img"
+    width="24"
+    height="24"
+    viewBox="0 0 100 100"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <title>warning</title>
+    <path
+      fill="currentColor"
+      d="M50 91.667c23.012 0 41.667-18.655 41.667-41.667S73.012 8.333 50 8.333 8.333 26.988 8.333 50 26.988 91.667 50 91.667zm0-75c18.409 0 33.333 14.924 33.333 33.333S68.409 83.333 50 83.333 16.667 68.409 16.667 50c.021-18.401 14.932-33.312 33.331-33.333H50z"
+    />
+    <path
+      fill="currentColor"
+      d="M54.167 54.167h-8.333v-25h8.333v25zm-9.375 12.5a5.208 5.208 0 1110.417 0 5.208 5.208 0 01-10.417 0z"
+    ></path>
+  </svg>
+</template>
+<script setup lang="ts"></script>
