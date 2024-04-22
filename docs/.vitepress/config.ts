6aa5052bc2a9f2435d8f773dff31b7a7d513db54
langehm <38280183+langehm@users.noreply.github.com>
2024-04-22 11:15:36 +0200

    :bento: Added Comment Component to index files #30-adding-comment-component

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,34 +1,36 @@
-const path = require('path')
+const path = require("path")
 
 module.exports = {
-  title: 'muc-patternlab-vue',
-  description: 'Vue Component Library for MDE5 Patternlab.',
-  base: '/muc-patternlab-vue/',
+  title: "muc-patternlab-vue",
+  description: "Vue Component Library for MDE5 Patternlab.",
+  base: "/muc-patternlab-vue/",
   themeConfig: {
-    repo: 'https://github.com/it-at-m/muc-patternlab-vue',
+    repo: "https://github.com/it-at-m/muc-patternlab-vue",
     sidebar: [
       {
-        text: 'Introduction',
+        text: "Introduction",
         children: [
-          { text: 'What is muc-patternlab-vue?', link: '/' },
-          { text: 'Getting Started', link: '/guide/' },
+          { text: "What is muc-patternlab-vue?", link: "/" },
+          { text: "Getting Started", link: "/guide/" },
         ],
-      }, {
-        text: 'Components',
+      },
+      {
+        text: "Components",
         children: [
-          { text: 'MucBanner', link: '/components/muc-banner' },
-          { text: 'MucButton', link: '/components/muc-button' },
-          { text: 'MucIntro', link: '/components/muc-intro' },
+          { text: "MucBanner", link: "/components/muc-banner" },
+          { text: "MucButton", link: "/components/muc-button" },
+          { text: "MucIntro", link: "/components/muc-intro" },
+          { text: "MucComment", link: "/components/muc-comment" },
         ],
-      }
+      },
     ],
   },
   vite: {
     resolve: {
       alias: {
-        'muc-patternlab-vue': path.resolve(__dirname, '../../src'),
+        "muc-patternlab-vue": path.resolve(__dirname, "../../src"),
       },
-      dedupe: ['vue'], // avoid error when using dependencies that also use Vue
-    }
-  }
+      dedupe: ["vue"], // avoid error when using dependencies that also use Vue
+    },
+  },
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
-import MucButton from "./Button/MucButton.vue";
-import MucBanner from "./Banner/MucBanner.vue";
-import MucIntro from "./Intro/MucIntro.vue";
+import MucButton from "./Button/MucButton.vue"
+import MucBanner from "./Banner/MucBanner.vue"
+import MucIntro from "./Intro/MucIntro.vue"
+import MucComment from "./Comment/MucComment.vue"
+import MucCommentText from "./Comment/MucCommentText.vue"
 
-export { MucButton, MucBanner, MucIntro };
+export { MucButton, MucBanner, MucIntro, MucComment, MucCommentText }
