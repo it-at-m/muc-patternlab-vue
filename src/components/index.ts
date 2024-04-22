df49b6fab6adc284b019a37b0be6827d2083dfa1
langehm <38280183+langehm@users.noreply.github.com>
2024-04-22 18:23:14 +0200

    corrected index.ts #32-use-the-updated-refarch-prettier-config

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import MucBanner from "./Banner/MucBanner.vue";
 import MucButton from "./Button/MucButton.vue";
+import MucCallout from "./Callout/MucCallout.vue";
 import MucIntro from "./Intro/MucIntro.vue";
 
-export { MucButton, MucBanner, MucIntro };
+export { MucButton, MucBanner, MucIntro, MucCallout };
