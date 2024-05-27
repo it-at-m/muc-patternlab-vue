57a156e07961bd5c7948f2d8afeaaf1a34d0c882
langehm <38280183+langehm@users.noreply.github.com>
2024-05-27 23:17:12 +0200

    :sparkles: added muc-icon #95

---
+++ src/components/Icon/MucIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    aria-hidden="true"
+    class="m-button__icon"
+    :style="{ color: color }"
+  >
+    <use :xlink:href="'#icon-' + icon" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  icon: string;
+  color?: string;
+}>();
+</script>
