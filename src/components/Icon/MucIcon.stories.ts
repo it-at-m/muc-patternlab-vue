5fd8df757cf58a684469857ca982b5207dbdb6ca
langehm <38280183+langehm@users.noreply.github.com>
2024-05-27 23:17:52 +0200

    :memo: added muc-icon documentation #95

---
+++ src/components/Icon/MucIcon.stories.ts
@@ -0,0 +1,665 @@
+import { fn } from "@storybook/test";
+
+import MucIcon from "./MucIcon.vue";
+
+export default {
+  component: MucIcon,
+  title: "MucIcon",
+  tags: ["autodocs"],
+  // 👇 Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked
+  args: { onClick: fn() },
+  parameters: {
+    docs: {
+      description: {
+        component: `The \`muc-icon\` component is a wrapper commponent for all icons in the patternlab.
+
+[🔗 Patternlab-Docs](https://patternlab.muenchen.space/?p=viewall-guidelines-icons)
+`,
+      },
+    },
+  },
+};
+
+export const Youtube = {
+  args: {
+    icon: "youtube",
+    color: "red",
+  },
+};
+
+export const Whatsapp = {
+  args: {
+    icon: "whatsapp",
+    color: "green",
+  },
+};
+
+export const Web = {
+  args: {
+    icon: "web",
+  },
+};
+
+export const Weather = {
+  args: {
+    icon: "weather",
+  },
+};
+
+export const Warning = {
+  args: {
+    icon: "warning",
+  },
+};
+
+export const VideoCamera = {
+  args: {
+    icon: "video-camera",
+  },
+};
+
+export const User = {
+  args: {
+    icon: "user",
+  },
+};
+
+export const UserGroup = {
+  args: {
+    icon: "user-group",
+  },
+};
+
+export const Upload = {
+  args: {
+    icon: "upload",
+  },
+};
+
+export const Unlock = {
+  args: {
+    icon: "unlock",
+  },
+};
+
+export const UnfollowUser = {
+  args: {
+    icon: "unfollow-user",
+  },
+};
+
+export const Twitter = {
+  args: {
+    icon: "twitter",
+  },
+};
+
+export const TwitterX = {
+  args: {
+    icon: "twitter-x",
+  },
+};
+
+export const Trash = {
+  args: {
+    icon: "trash",
+  },
+};
+
+export const Time = {
+  args: {
+    icon: "time",
+  },
+};
+
+export const Ticket = {
+  args: {
+    icon: "ticket",
+  },
+};
+
+export const Telephone = {
+  args: {
+    icon: "telephone",
+  },
+};
+
+export const Stopwatch = {
+  args: {
+    icon: "stopwatch",
+  },
+};
+
+export const Star = {
+  args: {
+    icon: "star",
+  },
+};
+
+export const Speaker = {
+  args: {
+    icon: "speaker",
+  },
+};
+
+export const SolidStar = {
+  args: {
+    icon: "solid-star",
+  },
+};
+
+export const SingIn = {
+  args: {
+    icon: "sing-in",
+  },
+};
+
+export const SignOut = {
+  args: {
+    icon: "sign-out",
+  },
+};
+
+export const SignLanguage = {
+  args: {
+    icon: "sign-language",
+  },
+};
+
+export const Shrink = {
+  args: {
+    icon: "shrink",
+  },
+};
+
+export const ShareIos = {
+  args: {
+    icon: "share-ios",
+  },
+};
+
+export const ShareAndorid = {
+  args: {
+    icon: "share-andorid",
+  },
+};
+
+export const Settings = {
+  args: {
+    icon: "settings",
+  },
+};
+
+export const Search = {
+  args: {
+    icon: "search",
+  },
+};
+
+export const Route = {
+  args: {
+    icon: "route",
+  },
+};
+
+export const RouteAlt = {
+  args: {
+    icon: "route-alt",
+  },
+};
+
+export const Privacy = {
+  args: {
+    icon: "privacy",
+  },
+};
+
+export const Plus = {
+  args: {
+    icon: "plus",
+  },
+};
+
+export const Play = {
+  args: {
+    icon: "play",
+  },
+};
+
+export const PlaySquare = {
+  args: {
+    icon: "play-square",
+  },
+};
+
+export const PlayCircle = {
+  args: {
+    icon: "play-circle",
+  },
+};
+
+export const Place = {
+  args: {
+    icon: "place",
+  },
+};
+
+export const Pause = {
+  args: {
+    icon: "pause",
+  },
+};
+
+export const Movie = {
+  args: {
+    icon: "movie",
+  },
+};
+
+export const Minus = {
+  args: {
+    icon: "minus",
+  },
+};
+
+export const Menu = {
+  args: {
+    icon: "menu",
+  },
+};
+
+export const Mde_iconParking = {
+  args: {
+    icon: "mde_icon-parking",
+  },
+};
+
+export const Map = {
+  args: {
+    icon: "map",
+  },
+};
+
+export const MapPin = {
+  args: {
+    icon: "map-pin",
+  },
+};
+
+export const Mail = {
+  args: {
+    icon: "mail",
+  },
+};
+
+export const Lock = {
+  args: {
+    icon: "lock",
+  },
+};
+
+export const LocationIos = {
+  args: {
+    icon: "location-ios",
+  },
+};
+
+export const LocationAndroid = {
+  args: {
+    icon: "location-android",
+  },
+};
+
+export const List = {
+  args: {
+    icon: "list",
+  },
+};
+
+export const Language = {
+  args: {
+    icon: "language",
+  },
+};
+
+export const Instagram = {
+  args: {
+    icon: "instagram",
+  },
+};
+
+export const Information = {
+  args: {
+    icon: "information",
+  },
+};
+
+export const ImageGallery = {
+  args: {
+    icon: "image-gallery",
+  },
+};
+
+export const Hourglass = {
+  args: {
+    icon: "hourglass",
+  },
+};
+
+export const HourglassStart = {
+  args: {
+    icon: "hourglass-start",
+  },
+};
+
+export const HourglassSolid = {
+  args: {
+    icon: "hourglass-solid",
+  },
+};
+
+export const HourglassEnd = {
+  args: {
+    icon: "hourglass-end",
+  },
+};
+
+export const Home = {
+  args: {
+    icon: "home",
+  },
+};
+
+export const Heart = {
+  args: {
+    icon: "heart",
+  },
+};
+
+export const HalfStar = {
+  args: {
+    icon: "half-star",
+  },
+};
+
+export const Fsk6 = {
+  args: {
+    icon: "fsk-6",
+  },
+};
+
+export const Fsk18 = {
+  args: {
+    icon: "fsk-18",
+  },
+};
+
+export const Fsk16 = {
+  args: {
+    icon: "fsk-16",
+  },
+};
+
+export const Fsk12 = {
+  args: {
+    icon: "fsk-12",
+  },
+};
+
+export const Fsk0 = {
+  args: {
+    icon: "fsk-0",
+  },
+};
+
+export const FollowUser = {
+  args: {
+    icon: "follow-user",
+  },
+};
+
+export const Floppy = {
+  args: {
+    icon: "floppy",
+  },
+};
+
+export const Filter = {
+  args: {
+    icon: "filter",
+  },
+};
+
+export const File = {
+  args: {
+    icon: "file",
+  },
+};
+
+export const Facebook = {
+  args: {
+    icon: "facebook",
+  },
+};
+
+export const Eye = {
+  args: {
+    icon: "eye",
+  },
+};
+
+export const EyeOff = {
+  args: {
+    icon: "eye-off",
+  },
+};
+
+export const ExtLink = {
+  args: {
+    icon: "ext-link",
+  },
+};
+
+export const Expand = {
+  args: {
+    icon: "expand",
+  },
+};
+
+export const EllipsisVertical = {
+  args: {
+    icon: "ellipsis-vertical",
+  },
+};
+
+export const EllipsisHorizontal = {
+  args: {
+    icon: "ellipsis-horizontal",
+  },
+};
+
+export const EasyToRead = {
+  args: {
+    icon: "easy-to-read",
+  },
+};
+
+export const Download = {
+  args: {
+    icon: "download",
+  },
+};
+
+export const DoubleUp = {
+  args: {
+    icon: "double-up",
+  },
+};
+
+export const DoubleRight = {
+  args: {
+    icon: "double-right",
+  },
+};
+
+export const DoubleLeft = {
+  args: {
+    icon: "double-left",
+  },
+};
+
+export const DoubleDown = {
+  args: {
+    icon: "double-down",
+  },
+};
+
+export const DeskBell = {
+  args: {
+    icon: "desk-bell",
+  },
+};
+
+export const CopyLink = {
+  args: {
+    icon: "copy-link",
+  },
+};
+
+export const Close = {
+  args: {
+    icon: "close",
+  },
+};
+
+export const ChevronUp = {
+  args: {
+    icon: "chevron-up",
+  },
+};
+
+export const ChevronRight = {
+  args: {
+    icon: "chevron-right",
+  },
+};
+
+export const ChevronLeft = {
+  args: {
+    icon: "chevron-left",
+  },
+};
+
+export const ChevronDown = {
+  args: {
+    icon: "chevron-down",
+  },
+};
+
+export const Check = {
+  args: {
+    icon: "check",
+  },
+};
+
+export const Camera = {
+  args: {
+    icon: "camera",
+  },
+};
+
+export const Calendar = {
+  args: {
+    icon: "calendar",
+  },
+};
+
+export const CalendarWeekend = {
+  args: {
+    icon: "calendar-weekend",
+  },
+};
+
+export const CalendarWeek = {
+  args: {
+    icon: "calendar-week",
+  },
+};
+
+export const CalendarUp = {
+  args: {
+    icon: "calendar-up",
+  },
+};
+
+export const CalendarRight = {
+  args: {
+    icon: "calendar-right",
+  },
+};
+
+export const CalendarLeft = {
+  args: {
+    icon: "calendar-left",
+  },
+};
+
+export const CalendarDown = {
+  args: {
+    icon: "calendar-down",
+  },
+};
+
+export const CalendarDay = {
+  args: {
+    icon: "calendar-day",
+  },
+};
+
+export const Bullet = {
+  args: {
+    icon: "bullet",
+  },
+};
+
+export const Battery = {
+  args: {
+    icon: "battery",
+  },
+};
+
+export const ArrowUp = {
+  args: {
+    icon: "arrow-up",
+  },
+};
+
+export const ArrowRight = {
+  args: {
+    icon: "arrow-right",
+  },
+};
+
+export const ArrowLeft = {
+  args: {
+    icon: "arrow-left",
+  },
+};
+
+export const ArrowDown = {
+  args: {
+    icon: "arrow-down",
+  },
+};
+
+export const Accessibility = {
+  args: {
+    icon: "accessibility",
+  },
+};
+
+export const AccessibilityAlt = {
+  args: {
+    icon: "accessibility-alt",
+  },
+};
