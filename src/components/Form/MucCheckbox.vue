903aec663fa9ab2b96a9c4285182fd139cadc3bb
langehm <38280183+langehm@users.noreply.github.com>
2024-06-06 23:56:34 +0200

    :memo: updated code documentation #51

--- src/components/Form/MucCheckbox.vue
+++ src/components/Form/MucCheckbox.vue
@@ -25,17 +25,25 @@
 const checkBoxValue = defineModel<boolean>("modelValue", { default: false });
 
 defineProps<{
+  /**
+   * Label is displayed to the right of the checkbox as information for the user.
+   */
   label: string;
 }>();
 
 const emit = defineEmits<{
+  /**
+   * Emits every time the checkbox or the label is clicked - thus switching the state.
+   * @param e emits the click event.
+   */
   (e: "click"): void;
 }>();
 
+/**
+ * Called upon clicking the checkbox or label, switches the state of the checkbox and emits the event.
+ */
 const clickedCheckbox = () => {
   checkBoxValue.value = !checkBoxValue.value;
   emit("click");
 };
 </script>
-
-<style scoped></style>
