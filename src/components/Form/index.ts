7a4bcd197af0917f968b7d99f3a0557a0cd4706b
langehm <38280183+langehm@users.noreply.github.com>
2024-07-18 17:04:22 +0200

    Added import of components #51

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -1,3 +1,17 @@
+import MucCheckbox from "./MucCheckbox.vue";
+import MucCheckboxGroup from "./MucCheckboxGroup.vue";
 import MucForm from "./MucInput.vue";
+import MucInput from "./MucInput.vue";
+import MucRadioButton from "./MucRadioButton.vue";
+import MucRadioButtonGroup from "./MucRadioButtonGroup.vue";
+import MucTextArea from "./MucTextArea.vue";
 
-export { MucForm };
+export {
+  MucForm,
+  MucCheckbox,
+  MucCheckboxGroup,
+  MucRadioButtonGroup,
+  MucInput,
+  MucRadioButton,
+  MucTextArea,
+};
