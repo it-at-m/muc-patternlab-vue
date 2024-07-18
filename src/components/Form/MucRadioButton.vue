9fd3a6e244c0ba7a054432d7b97906577bce8f14
langehm <38280183+langehm@users.noreply.github.com>
2024-07-18 17:01:16 +0200

    :memo: added jsdoc to radiobutton #51

--- src/components/Form/MucRadioButton.vue
+++ src/components/Form/MucRadioButton.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="m-radios__item">
+  <div
+    class="m-radios__item"
+    v-if="isInRadioButtonGroup"
+  >
     <input
       class="m-radios__input"
       type="radio"
@@ -20,15 +23,30 @@
 </template>
 
 <script setup lang="ts">
-import { computed, inject, onMounted } from "vue";
+import { computed, inject } from "vue";
 
 import { RadioButtonGroupKey } from "./RadioButtonTypes";
 
 const props = withDefaults(
   defineProps<{
+    /**
+     * value for this radiobutton
+     */
     value: string;
+
+    /**
+     * Optional label shown behind the radiobutton
+     */
     label?: string;
+
+    /**
+     * Optional hint shown beneath the radiobutton
+     */
     hint?: string;
+
+    /**
+     * Optionally disable this specific radiobutton
+     */
     disabled?: boolean;
   }>(),
   {
@@ -36,13 +54,28 @@ const props = withDefaults(
   }
 );
 
+/**
+ * Data provided by the radio-button-group
+ */
 const parentData = inject(RadioButtonGroupKey);
 
+/**
+ * Function called upon clicking this radiobutton
+ */
 const clicked = () => parentData?.set(props.value);
 
+/**
+ * Computed property if this radiobutton is checked or not
+ */
 const isChecked = computed(() => parentData?.modelValue.value === props.value);
 
+/**
+ * Computed property if this radiobutton should be disabled
+ */
 const isDisabled = computed(() => props.disabled || parentData?.disabled.value);
 
+/**
+ * Determines if  this radiobutton is used inside a radiobutton-group
+ */
 const isInRadioButtonGroup = computed(() => !!parentData?.disabled ?? false);
 </script>
