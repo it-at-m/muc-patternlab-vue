a71a56787fbb5e32b91d5e1ba7108d86f5f90249
jannik.lange <jannik.lange@muenchen.de>
2024-08-07 14:50:31 +0200

    :rotating_light: linted #175

--- src/components/Form/MucRadioButtonGroup.vue
+++ src/components/Form/MucRadioButtonGroup.vue
@@ -16,7 +16,10 @@
 <script setup lang="ts">
 import { provide, readonly, toRef } from "vue";
 
-import { RadioButtonGroupKey, RadioButtonValueTypes } from "./MucRadioButtonTypes";
+import {
+  RadioButtonGroupKey,
+  RadioButtonValueTypes,
+} from "./MucRadioButtonTypes";
 
 /**
  * exposed two-way binding of the currently selected radiobuttons-value
