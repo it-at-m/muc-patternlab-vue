b974ac7593222ddca5d6b6ed67166a8082a58848
jannik.lange <jannik.lange@muenchen.de>
2024-08-13 17:13:54 +0200

    :sparkles: added muclink component #182

---
+++ src/components/Link/MucLink.vue
@@ -0,0 +1,44 @@
+<template>
+  <a
+    :href="href"
+    :target="target"
+    class="m-link"
+    :class="reversedUnderlineClass"
+  >
+    {{ label }}
+    <slot name="icon">
+      <muc-icon
+        v-if="icon"
+        :icon="icon"
+        class="icon icon--after"
+      />
+    </slot>
+  </a>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { MucIcon } from "../Icon";
+
+const props = withDefaults(
+  defineProps<{
+    label: string;
+    href?: string;
+    icon?: string;
+    target?: string;
+    noUnderline?: boolean;
+  }>(),
+  {
+    noUnderline: false,
+    href: "#",
+    target: "_blank",
+  }
+);
+
+const reversedUnderlineClass = computed(() =>
+  props.noUnderline ? "m-link--reversed-underline" : ""
+);
+</script>
+
+<style scoped></style>
