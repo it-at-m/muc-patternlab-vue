6f54bfb7baf10dfba53b2d02887d81cc9c95457c
jannik.lange <jannik.lange@muenchen.de>
2024-08-22 09:09:57 +0200

    some rework done #179

---
+++ src/components/Calendar/MucDayTile.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="{ false: 'selected' }">
+    {{ date }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { convertIndexToDate } from "./MucCalendarType";
+
+const props = withDefaults(
+  defineProps<{
+    date: number;
+    index: number;
+    viewDate: Date;
+    selectedDate: Date;
+  }>(),
+  {}
+);
+
+const convertedDate = computed(() =>
+  convertIndexToDate(props.viewDate, props.date)
+);
+
+const isSelected = computed(() =>
+  convertedDate.value == props.selectedDate ? "selected" : ""
+);
+</script>
+
+<style scoped>
+.selected {
+  background: var(--color-brand-main-blue);
+  color: white;
+}
+</style>
