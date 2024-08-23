868485ead4725fa8c60e36897414d5ebd2613cb2
jannik.lange <jannik.lange@muenchen.de>
2024-08-23 11:32:37 +0200

    :beers:

--- src/components/Calendar/MucDayTile.vue
+++ src/components/Calendar/MucDayTile.vue
@@ -1,36 +1,53 @@
 <template>
-  <div :class="{ false: 'selected' }">
-    {{ date }}
+  <div
+    v-if="isCurrMonth || !onlyCurrMonth"
+    @click="clicked"
+    :class="{
+      'off-month': !isCurrMonth,
+      selected: isSelected,
+    }"
+  >
+    {{ date.getDate() }}
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
 
-import { convertIndexToDate } from "./MucCalendarType";
+import { compareDates } from "./MucCalendarType";
 
 const props = withDefaults(
   defineProps<{
-    date: number;
-    index: number;
+    date: Date;
     viewDate: Date;
     selectedDate: Date;
+    onlyCurrMonth?: boolean;
   }>(),
-  {}
+  {
+    onlyCurrMonth: false,
+  }
 );
 
-const convertedDate = computed(() =>
-  convertIndexToDate(props.viewDate, props.date)
+const emit = defineEmits<{
+  click: [date: Date];
+}>();
+
+const isSelected = computed(() => compareDates(props.selectedDate, props.date));
+
+const isCurrMonth = computed(
+  () => props.date.getMonth() === props.viewDate.getMonth()
 );
 
-const isSelected = computed(() =>
-  convertedDate.value == props.selectedDate ? "selected" : ""
-);
+const clicked = () => emit("click", props.date);
 </script>
 
 <style scoped>
 .selected {
   background: var(--color-brand-main-blue);
   color: white;
+}
+
+.off-month {
+  color: var(--color-neutrals-blue);
 }
 </style>
