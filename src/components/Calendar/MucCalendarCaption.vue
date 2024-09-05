687db80b57507d777ab0169f069c5f7fcb466289
jannik.lange <jannik.lange@muenchen.de>
2024-09-05 15:49:25 +0200

    save

---
+++ src/components/Calendar/MucCalendarCaption.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div v-for="month in months">{{ month }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const months = ["Januar", "Februar", "März", "April"];
+</script>
+
+<style scoped></style>
