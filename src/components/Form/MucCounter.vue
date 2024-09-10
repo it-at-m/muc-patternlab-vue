bbfe4135f274dec76ab563d0fc105892cf6d9534
Julia Lehmann <julia.lehmann@muenchen.de>
2024-09-10 15:51:37 +0200

    186: fix format

--- src/components/Form/MucCounter.vue
+++ src/components/Form/MucCounter.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="wrapper">
-    <MucButton v-on:click="clickedMinus" variant="secondary" :disabled="disableMinus">
+    <MucButton
+      v-on:click="clickedMinus"
+      variant="secondary"
+      :disabled="disableMinus"
+    >
       <template #default><muc-icon icon="minus" /></template>
     </MucButton>
     <p>
-        <strong class="centered-text" style="color: #005a9f;">
-          {{ modelValue }}
-        </strong>
+      <strong
+        class="centered-text"
+        style="color: #005a9f"
+      >
+        {{ modelValue }}
+      </strong>
     </p>
-    <MucButton v-on:click="clickedPlus" variant="secondary" :disabled="disablePlus">
+    <MucButton
+      v-on:click="clickedPlus"
+      variant="secondary"
+      :disabled="disablePlus"
+    >
       <template #default><muc-icon icon="plus" /></template>
     </MucButton>
-    <p v-if="link" >
+    <p v-if="link">
       <label class="centered-text">
-        <muc-link :label="label" :href="link"></muc-link>
+        <muc-link
+          :label="label"
+          :href="link"
+        ></muc-link>
       </label>
     </p>
 
@@ -26,10 +40,11 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
+
 import { MucButton } from "../Button";
 import { MucIcon } from "../Icon";
 import { MucLink } from "../Link";
-import { computed } from "vue";
 
 /**
  * Input value from the counter component.
@@ -37,35 +52,35 @@ import { computed } from "vue";
 const modelValue = defineModel<number>({ default: 0 });
 
 const props = withDefaults(
-    defineProps<{
-      /**
-       * Label shown behind the counter
-       */
-      label: string;
+  defineProps<{
+    /**
+     * Label shown behind the counter
+     */
+    label: string;
 
-      /**
-       * Optional minimum of counter
-       */
-      min?: number;
+    /**
+     * Optional minimum of counter
+     */
+    min?: number;
 
-      /**
-       * Optional maximum of counter
-       */
-      max?: number;
+    /**
+     * Optional maximum of counter
+     */
+    max?: number;
 
-      /**
-       * Optional link for label
-       */
-      link?: string;
+    /**
+     * Optional link for label
+     */
+    link?: string;
 
-      /**
-       * Optionally disable this specific counter
-       */
-      disabled?: boolean;
-    }>(),
-    {
-      disabled: false,
-    }
+    /**
+     * Optionally disable this specific counter
+     */
+    disabled?: boolean;
+  }>(),
+  {
+    disabled: false,
+  }
 );
 
 /**
@@ -80,18 +95,19 @@ const clickedMinus = () => modelValue.value--;
 /**
  * Computed property if this plus button should be disabled
  */
-const disablePlus = computed(() =>
-  (!!props.max && !(modelValue.value < props.max)) || props.disabled
+const disablePlus = computed(
+  () => (!!props.max && !(modelValue.value < props.max)) || props.disabled
 );
 
 /**
  * Computed property if this minus button should be disabled
  */
-const disableMinus = computed(() =>
-    modelValue.value == 0 || (!!props.min && !(modelValue.value > props.min)) || props.disabled
+const disableMinus = computed(
+  () =>
+    modelValue.value == 0 ||
+    (!!props.min && !(modelValue.value > props.min)) ||
+    props.disabled
 );
-
-
 </script>
 
 <style scoped>
