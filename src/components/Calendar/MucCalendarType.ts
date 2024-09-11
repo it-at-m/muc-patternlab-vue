9d905e8e6b0a8c7142fe13e5dda9abeb3aaa1cf3
jannik.lange <jannik.lange@muenchen.de>
2024-09-11 16:42:07 +0200

    added documentation to types #179

--- src/components/Calendar/MucCalendarType.ts
+++ src/components/Calendar/MucCalendarType.ts
@@ -1,14 +1,31 @@
 import { InjectionKey, Ref } from "vue";
 
+/**
+ * Different possible types of date selection for the calendar
+ */
 type CalendarTypes = "single" | "multiple" | "range";
 
+/**
+ * Types of the view displayed for selection
+ */
+type ViewType = "day" | "month" | "year";
+
+/**
+ * Selection type for range type
+ */
 interface MucDateRange {
   from: null | Date;
   to: null | Date;
 }
 
+/**
+ * Datatypes for modelvalue of the calendar
+ */
 type MucCalendarSelected = null | Date | Date[] | MucDateRange;
 
+/**
+ * Injection type for date selection component
+ */
 type MucCalendarInjection = {
   viewDate: Readonly<Ref<Date>>;
   selectedDate: Readonly<Ref<MucCalendarSelected>>;
@@ -21,18 +38,40 @@ export type {
   MucCalendarSelected,
   CalendarTypes,
   MucCalendarInjection,
+  ViewType,
 };
 
+/**
+ * Key for injecting MucCalendar data into components.
+ * @type {InjectionKey<MucCalendarInjection>}
+ */
 const MucCalendarKey: InjectionKey<MucCalendarInjection> =
   Symbol("mucCalendar");
 
+/**
+ * Checks if two dates are equal (same day, month, and year).
+ * @param date1 - The first date to compare.
+ * @param date2 - The second date to compare.
+ * @returns true if the dates are equal, otherwise false.
+ */
 const isEqualDates = (date1: Date, date2: Date) =>
   date1.getDate() === date2.getDate() &&
   date1.getMonth() === date2.getMonth() &&
   date1.getFullYear() === date2.getFullYear();
 
+/**
+ * Checks if the first date is after the second date.
+ * @param date1 - The first date to compare.
+ * @param date2 - The second date to compare.
+ * @returns true if the first date is after the second, otherwise false.
+ */
 const isDateAfterOther = (date1: Date, date2: Date) => date1 > date2;
 
+/**
+ * Checks if the given object is a MucDateRange (has 'from' and 'to' properties).
+ * @param {MucCalendarSelected} obj - The object to check.
+ * @returns {boolean} True if the object is a MucDateRange, otherwise false.
+ */
 const isMucDateRange = (obj: MucCalendarSelected): obj is MucDateRange =>
   obj !== null &&
   (obj as MucDateRange).from !== undefined &&
