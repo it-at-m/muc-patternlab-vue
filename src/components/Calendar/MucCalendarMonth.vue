329823b8ca9e0ccc151fdf92696553541a4403bc
jannik.lange <jannik.lange@muenchen.de>
2024-09-11 16:42:18 +0200

    added documentation to month selection #179

--- src/components/Calendar/MucCalendarMonth.vue
+++ src/components/Calendar/MucCalendarMonth.vue
@@ -18,19 +18,34 @@
 <script setup lang="ts">
 import { computed } from "vue";
 
+/**
+ * Chosen notation for date.toLocaleDateString() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#month
+ */
 const MONTH_NOTATION = "short";
 
 // TODO make external configuration for all components
+/**
+ * Locales for date output
+ */
 const LOCALES = "de-DE";
 
 const props = defineProps<{
+  /**
+   * current viewDate for the selection
+   */
   viewDate: Date;
 }>();
 
 const emit = defineEmits<{
+  /**
+   * Event emited after selecting a month
+   */
   clicked: [date: Date];
 }>();
 
+/**
+ * Computed months to be displayed in the selection
+ */
 const computedMonths = computed(() =>
   Array.from(
     { length: 12 },
@@ -38,9 +53,17 @@ const computedMonths = computed(() =>
   )
 );
 
+/**
+ * String displayment for a given date
+ * @param date to be displayed
+ */
 const monthDisplayment = (date: Date) =>
   date.toLocaleDateString(LOCALES, { month: MONTH_NOTATION });
 
+/**
+ * Action upon selecting a month - triggers emit
+ * @param date selected month
+ */
 const clickedMonth = (date: Date) => emit("clicked", date);
 </script>
 
