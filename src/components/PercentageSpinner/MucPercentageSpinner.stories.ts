8f81f361e397c5ef50718d395286823beb640c6b
Marko Streich <marko.streich@muenchen.de>
2024-09-27 15:51:27 +0200

    MPDZBS-842 introduce a spinner that represents a loading progress

---
+++ src/components/PercentageSpinner/MucPercentageSpinner.stories.ts
@@ -0,0 +1,17 @@
+import MucPercentageSpinner from "./MucPercentageSpinner.vue";
+
+export default {
+  component: MucPercentageSpinner,
+  title: "MucPercentageSpinner",
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component: `The \`muc-percentage-spinner\` is a spinner represents a loading progress.
+`,
+      },
+    },
+  },
+};
+
+export const Default = {};
