b1b2408e181bc006a8e70efffcf6bfab2c0650bf
langehm <38280183+langehm@users.noreply.github.com>
2024-10-10 14:28:57 +0200

    :sparkles: 179 graphical calendar (#214)
    * :beers: added components #179
    
    * :beers: save #179
    
    * :beers:
    
    * :lipstick: added some css #179
    
    * changed state to unofficial #179
    
    * added all days of prev, curr and next month #179
    
    * some rework done #179
    
    * :beers:
    
    * added functions for updating mv according to variant #179
    
    * added variant to mucdaytile #179
    
    * added examples #179
    
    * changed type to interface and added comparison methods #179
    
    * :bug: fixed issues with updating mv and display of selection #179
    
    * linted #179
    
    * changed and added properties #179
    
    * changed examples #179
    
    * changed props to provide/inject #179
    
    * save
    
    * disabled props was falsy set on story #179
    
    * outsourced view of day, month and year view into seperate components #179
    
    * inserted view logic #179
    
    * :lipstick: inserted animations #179
    
    * :lipstick: adjusted css styling #179
    
    * added documentation to calendar #179
    
    * added documentation to types #179
    
    * added documentation to month selection #179
    
    * fixed bug in styling and reverted styling into nonscoped block #179
    
    * reverted custom-style.css #179
    
    * :truck: renamed day view #179
    
    * :art: formated redundant const #179
    
    * added documentation to year selection #179
    
    * :recycle: removed unnecessary code #179
    
    * spelling mistake
    
    * added documentation to day selection #179
    
    * added documentation to day-tile #179
    
    * linted and formated #179
    
    * added example #179
    
    * :lipstick: improve animation code and performance
    
    no more stuttering and smoother
    
    * :recycle: fix linter
    
    * destructured props #179
    
    * :sparkles: animation can be disabled #179
    
    * :beer: added first functionality for allowing some dates #179
    
    * :beers: added components #179
    
    * :beers: save #179
    
    * :beers:
    
    * :lipstick: added some css #179
    
    * changed state to unofficial #179
    
    * added all days of prev, curr and next month #179
    
    * some rework done #179
    
    * :beers:
    
    * added functions for updating mv according to variant #179
    
    * added variant to mucdaytile #179
    
    * added examples #179
    
    * changed type to interface and added comparison methods #179
    
    * :bug: fixed issues with updating mv and display of selection #179
    
    * linted #179
    
    * changed and added properties #179
    
    * changed examples #179
    
    * changed props to provide/inject #179
    
    * save
    
    * disabled props was falsy set on story #179
    
    * outsourced view of day, month and year view into seperate components #179
    
    * inserted view logic #179
    
    * :lipstick: inserted animations #179
    
    * :lipstick: adjusted css styling #179
    
    * added documentation to calendar #179
    
    * added documentation to types #179
    
    * added documentation to month selection #179
    
    * fixed bug in styling and reverted styling into nonscoped block #179
    
    * reverted custom-style.css #179
    
    * :truck: renamed day view #179
    
    * :art: formated redundant const #179
    
    * added documentation to year selection #179
    
    * :recycle: removed unnecessary code #179
    
    * spelling mistake
    
    * added documentation to day selection #179
    
    * added documentation to day-tile #179
    
    * linted and formated #179
    
    * added example #179
    
    * :lipstick: improve animation code and performance
    
    no more stuttering and smoother
    
    * :recycle: fix linter
    
    * destructured props #179
    
    * :sparkles: animation can be disabled #179
    
    * :beer: added first functionality for allowing some dates #179
    
    * :sparkles: added allowDates functionality #179
    
    * :memo: updated documentation #179
    
    * :lipstick: outsourced stylesheet #179
    
    * :poop:
    
    * linted and formated #179
    
    * :green_heart: pipeline optional dependency
    
    * :green_heart: try to fix build by updating package-lock.json
    
    * :green_heart: revert optional dependency
    
    ---------
    
    Co-authored-by: jannik.lange <jannik.lange@muenchen.de>
    Co-authored-by: fabian.wilms <fabian.wilms@muenchen.de>

---
+++ src/components/Calendar/MucDayTile.vue
@@ -0,0 +1,134 @@
+<template>
+  <div
+    v-if="isCurrMonth || showAdjacentMonths"
+    @click="clicked"
+    :class="{
+      'disabled-tile': !mucCalData?.allowedDates(date),
+      'off-month': !isCurrMonth,
+      selected: isSelected,
+      'selected-range': isInRange,
+    }"
+  >
+    {{ date.getDate() }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, inject } from "vue";
+
+import {
+  isDateAfterOther,
+  isEqualDates,
+  isMucDateRange,
+  MucCalendarKey,
+} from "./MucCalendarType";
+
+const { date, showAdjacentMonths = false } = defineProps<{
+  /**
+   * Date of for this tile to be displayed
+   */
+  date: Date;
+
+  /**
+   * Determines if this date should be shown or not, depending on the month - defaults to false
+   */
+  showAdjacentMonths?: boolean;
+}>();
+
+/**
+ * Injection of data needed for styling calculations
+ */
+const mucCalData = inject(MucCalendarKey);
+
+const emit = defineEmits<{
+  /**
+   * Event emited after selecting a day
+   */
+  click: [date: Date];
+}>();
+
+/**
+ * Determines if the day-tile is selected or not depending on the current variant
+ */
+// eslint-disable-next-line vue/return-in-computed-property
+const isSelected = computed(() => {
+  if (mucCalData?.selectedDate.value === null) {
+    return false;
+  }
+
+  // variant single
+  if (mucCalData?.selectedDate.value instanceof Date) {
+    return isEqualDates(mucCalData?.selectedDate.value, date);
+  }
+
+  //variant multiple
+  if (Array.isArray(mucCalData?.selectedDate.value)) {
+    return mucCalData?.selectedDate.value.some((selected) =>
+      isEqualDates(selected, date)
+    );
+  }
+
+  //variant range
+  if (isMucDateRange(mucCalData!.selectedDate.value)) {
+    const { from, to } = mucCalData!.selectedDate.value;
+    return (from && isEqualDates(from, date)) || (to && isEqualDates(to, date));
+  }
+});
+
+/**
+ * Determines if this day-tile is inside the range of selected dates on type variant
+ */
+const isInRange = computed(() => {
+  if (
+    mucCalData?.variant.value === "range" &&
+    isMucDateRange(mucCalData!.selectedDate.value)
+  ) {
+    return (
+      mucCalData?.selectedDate.value.from !== null &&
+      mucCalData?.selectedDate.value.to !== null &&
+      isDateAfterOther(date, mucCalData!.selectedDate.value.from) &&
+      isDateAfterOther(mucCalData!.selectedDate.value.to, date)
+    );
+  }
+
+  return false;
+});
+
+/**
+ * Is this day-tile in the current month or in the adjacent months
+ */
+const isCurrMonth = computed(
+  () => date.getMonth() === mucCalData?.viewDate.value.getMonth()
+);
+
+/**
+ * Action upon selecting a day - triggers emit
+ */
+const clicked = () => {
+  if (mucCalData?.allowedDates(date)) {
+    emit("click", date);
+  }
+};
+</script>
+
+<style scoped>
+@import "MucCalendar.css";
+
+.selected {
+  background: var(--color-brand-main-blue);
+  color: white;
+}
+
+.disabled-tile {
+  filter: grayscale(80%);
+  color: lightgray;
+  cursor: default !important;
+}
+
+.selected-range {
+  background: var(--color-neutrals-blue-xlight);
+}
+.off-month {
+  color: var(--color-neutrals-blue);
+}
+</style>
