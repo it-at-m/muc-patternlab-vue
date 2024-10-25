5d361b6dbdf1ef0c7612b1752cb8e6b1d581b4d5
lehju <122289623+lehju@users.noreply.github.com>
2024-10-25 07:55:09 +0200

    ✨ 181 stepper component (#279)
    * 181: add stepper component
    
    * 181: add mobile and screenreader
    
    * 181: add comments
    
    * 181: format code
    
    * 181: Change example to english
    
    * 181: Add MucCalendar export
    
    * 181: format index.ts

---
+++ src/components/Stepper/MucStepper.stories.ts
@@ -0,0 +1,45 @@
+import MucStepper from "./MucStepper.vue";
+
+export default {
+  component: MucStepper,
+  title: "MucStepper",
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component: `The MucStepper offers the functionality of a progress bar.
+
+[🔗 Patternlab-Docs](https://patternlab.muenchen.space/?p=components-form-steps)
+`,
+      },
+    },
+  },
+};
+
+export const Default = {
+  args: {
+    stepItems: [
+      {
+        id: "1",
+        label: "Order",
+        icon: "shopping-cart",
+      },
+      {
+        id: "2",
+        label: "Delivery",
+        icon: "calendar",
+      },
+      {
+        id: "3",
+        label: "Contact",
+        icon: "mail",
+      },
+      {
+        id: "4",
+        label: "Overview",
+        icon: "information",
+      },
+    ],
+    activeItem: "2",
+  },
+};
