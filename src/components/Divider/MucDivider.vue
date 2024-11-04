408e725fad3b8a05a4b963eb956face77a0d0000
langehm <langehm@users.noreply.github.com>
2024-11-04 12:20:15 +0100

    :recycle: moved css class into the component #183

--- src/components/Divider/MucDivider.vue
+++ src/components/Divider/MucDivider.vue
@@ -1,3 +1,11 @@
 <template>
   <div class="muc-divider" />
 </template>
+
+<style scoped>
+.muc-divider {
+  align-self: stretch;
+  height: 0;
+  border-bottom: 1px var(--color-neutrals-blue) solid;
+}
+</style>
