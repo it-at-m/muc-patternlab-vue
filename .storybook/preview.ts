6ea35c0403e32b888b6ea54b51a7077b6d7802a6
langehm <langehm@users.noreply.github.com>
2024-11-25 18:24:31 +0100

    included sass into storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,7 @@
 import "../public/assets/temporary/muenchende-style.css";
 import "../public/assets/temporary/custom-style.css";
 import "../public/assets/temporary/muenchende-fontfaces.css";
+import "../public/assets/temporary/central_css.scss";
 
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
