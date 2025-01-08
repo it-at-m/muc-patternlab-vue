0cfeb05018f2d3d4b91ad8440568f25479b325ef
lehju <julia.lehmann@muenchen.de>
2025-01-08 09:07:15 +0100

    272: format code

--- src/components/Slider/SliderOptions.ts
+++ src/components/Slider/SliderOptions.ts
@@ -1,33 +1,33 @@
 import type { Options } from "@splidejs/splide";
 
 export const sliderOptions: Options = {
-    autoplay: false,
-    keyboard: true,
-    slideFocus: true,
-    lazyLoad: false,
-    arrows: false,
-    perMove: 1,
-    gap: "0",
-    type: "slide",
-    perPage: 1,
-    pagination: false,
-    speed: 350,
-    drag: true,
-    swipe: true,
-    swipeThreshold: 50,
-    mediaQuery: "min",
-    breakpoints: {
-        1: {
-            perPage: 1,
-            gap: "1.5rem",
-        },
-        768: {
-            perPage: 1,
-            gap: "1.5rem",
-        },
-        1200: {
-            perPage: 1,
-            gap: "2rem",
-        },
+  autoplay: false,
+  keyboard: true,
+  slideFocus: true,
+  lazyLoad: false,
+  arrows: false,
+  perMove: 1,
+  gap: "0",
+  type: "slide",
+  perPage: 1,
+  pagination: false,
+  speed: 350,
+  drag: true,
+  swipe: true,
+  swipeThreshold: 50,
+  mediaQuery: "min",
+  breakpoints: {
+    1: {
+      perPage: 1,
+      gap: "1.5rem",
     },
+    768: {
+      perPage: 1,
+      gap: "1.5rem",
+    },
+    1200: {
+      perPage: 1,
+      gap: "2rem",
+    },
+  },
 };
