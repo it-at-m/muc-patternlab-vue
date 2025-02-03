999d2aed18a4212b68c65f6ce2bb445ccac4bced
jannik.lange <jannik.lange@muenchen.de>
2025-02-03 16:55:01 +0100

    added sass back after merge and adjusted preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,5 @@
-import "../public/assets/temporary/custom-style.css";
-import "../public/assets/temporary/muenchende-fontfaces.css";
+import "../public/assets/css/custom-style.css";
+import "../public/assets/css/central_css.scss";
 
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
