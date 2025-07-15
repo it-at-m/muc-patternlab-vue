4d97cb3c4e855715cc9c92d40f7a0c24c20df683
Dominik Grenz <dominik.grenz@muenchen.de>
2025-07-15 14:09:49 +0200

    Remove muc-intro-content

--- src/components/Intro/MucIntro.vue
+++ src/components/Intro/MucIntro.vue
@@ -63,7 +63,7 @@ defineSlots<{
               :height="size"
             />
           </div>
-          <div class="m-intro-vertical__grid-inner  muc-intro-content">
+          <div class="m-intro-vertical__grid-inner">
             <div>
               <p
                 v-if="tagline"
@@ -91,12 +91,6 @@ defineSlots<{
 </template>
 
 <style scoped>
-@media screen and (width >992px) {
-  .muc-intro-content {
-    width: 66.6%;
-    margin-left: 1px;
-  }
-}
 .muc-divider {
   margin-top: 8px;
   margin-bottom: 16px;
