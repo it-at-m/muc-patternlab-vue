c9aa1cd9b11740026fca8c359da23eed24b0dc01
matthilo96 <matthias.lorenz@muenchen.de>
2025-11-19 09:06:40 +0100

    Update src/components/PercentageSpinner/index.ts
    Co-authored-by: Fabian Wilms <10800158+FabianWilms@users.noreply.github.com>

--- src/components/PercentageSpinner/index.ts
+++ src/components/PercentageSpinner/index.ts
@@ -1,1 +1,4 @@
+/**
+ * @deprecated please use the new MucSpinner-Component instead. This export will be removed in upcoming versions.
+ */
 export { MucSpinner as MucPercentageSpinner } from "../Spinner";
