efa0cc10b91f4ae79b2c2f71a08f511d350c7fd0
lehju <122289623+lehju@users.noreply.github.com>
2026-02-26 11:15:28 +0100

    :boom: 687 enable multiple p tags in content slot of MucCallout - breaking change: the p tag must now be set by the user in the content slot (#695)
    * :boom: enable multiple p tags in content slot of MucCallout - breaking change: the p tag must now be set by the user in the content slot
    
    * :lipstick: delete duplicate
    
    * :lipstick: format code
    
    ---------
    
    Co-authored-by: matthilo96 <matthias.lorenz@muenchen.de>

--- src/components/Callout/MucCallout.stories.ts
+++ src/components/Callout/MucCallout.stories.ts
@@ -8,7 +8,7 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: `The \`muc-callout\` component is used to convey important information to the user through the use of contextual types, icons, and colors.
+        component: `The \`muc-callout\` component is used to convey important information to the user through the use of contextual types, icons, and colors. The text within the content slot can be structured using paragraphs and bullet points. Plain text must be enclosed in a \`<p>\` tag.
 
 [🔗 Patternlab-Docs](https://patternlab.muenchen.space/?p=viewall-components-callout)
 `,
@@ -17,37 +17,77 @@ export default {
   },
 };
 
-export const Default = {
-  args: {
-    header: "Information",
-    content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.",
-    type: "info",
-  },
-};
+export const Default = () => ({
+  components: { MucCallout },
+  template: `
+      <MucCallout
+          type="info"
+      >
+        <template #header>
+          Information
+        </template>
+        <template #content>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+          </p>
+        </template>
+      </MucCallout>
+    `,
+});
 
-export const Success = {
-  args: {
-    ...Default.args,
-    header: "Success",
-    type: "success",
-  },
-};
+export const Success = () => ({
+  components: { MucCallout },
+  template: `
+      <MucCallout
+          type="success"
+      >
+        <template #header>
+          Success
+        </template>
+        <template #content>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+          </p>
+        </template>
+      </MucCallout>
+    `,
+});
 
-export const Warning = {
-  args: {
-    ...Default.args,
-    header: "Warning",
-    type: "warning",
-  },
-};
+export const Warning = () => ({
+  components: { MucCallout },
+  template: `
+      <MucCallout
+          type="warning"
+      >
+        <template #header>
+          Warning
+        </template>
+        <template #content>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+          </p>
+        </template>
+      </MucCallout>
+    `,
+});
 
-export const Error = {
-  args: {
-    ...Default.args,
-    header: "Error",
-    type: "error",
-  },
-};
+export const Error = () => ({
+  components: { MucCallout },
+  template: `
+      <MucCallout
+          type="error"
+      >
+        <template #header>
+          Error
+        </template>
+        <template #content>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+          </p>
+        </template>
+      </MucCallout>
+    `,
+});
 
 export const WithButton = () => ({
   components: { MucCallout, MucButton },
@@ -59,11 +99,34 @@ export const WithButton = () => ({
          Button
         </template>
         <template #content>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua.
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+            dolore magna aliquyam erat, sed diam voluptua.
+          </p>
         </template>
         <template #buttons>
           <muc-button icon="arrow-right" icon-animated> Button</muc-button>
+        </template>
+      </MucCallout>
+    `,
+});
+
+export const WithMultiplePTags = () => ({
+  components: { MucCallout },
+  template: `
+      <MucCallout
+          type="info"
+      >
+        <template #header>
+          Information
+        </template>
+        <template #content>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+          </p>
         </template>
       </MucCallout>
     `,
